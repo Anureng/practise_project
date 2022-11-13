cbcbea74c1aa51bf9b576b367e7cc6443b45cdba
Anurag Nitin Sidhu <anuragsidhu72@gmail.com>
2022-11-13 11:07:45 +0530

    second

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import { BsStar } from "react-icons/Bs";
-import { ImStarHalf } from "react-icons/im";
-function Slider() {
-    return (
-        <div className='border flex border-black justify-center items-center'>
-            <div className='border border-yellow-700 w-3/5 md:w-1/5 lg:w-1/5 p-5 space-y-8'>
-                <div className='flex text-3xl space-x-2'>
-                    <BsStar className='decoration-[#50d71e]' />
-                    <BsStar className='decoration-[#50d71e]' />
-                    <BsStar className='decoration-[#50d71e]' />
-                    <BsStar className='decoration-[#50d71e]' />
-                    <ImStarHalf />
-                </div>
-                <div className='space-y-8'>
-                    <p className='text-xl font-bold'>Mailchimp is the best for small business newsletters</p>
-                    <p className='text-xl'>It allows you to easily design the newsletter and offers different templates. Very easy to drop in t…</p>
-                    <p className='underline underline-offset-1 cursor-pointer inline-block hover:bg-yellow-300'>Read Full Review</p>
-                    <p className='font-light'>Michelle Messenger Garrett Garrett Public Relations</p>
-                </div>
-            </div>
-        </div>
-    )
-}
+// import React from 'react'
+// import { BsStar } from "react-icons/Bs";
+// import { ImStarHalf } from "react-icons/im";
+// function Slider() {
+//     return (
+//         <div className='border flex border-black justify-center items-center'>
+//             <div className='border border-yellow-700 w-3/5 md:w-1/5 lg:w-1/5 p-5 space-y-8'>
+//                 <div className='flex text-3xl space-x-2'>
+//                     <BsStar className='decoration-[#50d71e]' />
+//                     <BsStar className='decoration-[#50d71e]' />
+//                     <BsStar className='decoration-[#50d71e]' />
+//                     <BsStar className='decoration-[#50d71e]' />
+//                     <ImStarHalf />
+//                 </div>
+//                 <div className='space-y-8'>
+//                     <p className='text-xl font-bold'>Mailchimp is the best for small business newsletters</p>
+//                     <p className='text-xl'>It allows you to easily design the newsletter and offers different templates. Very easy to drop in t…</p>
+//                     <p className='underline underline-offset-1 cursor-pointer inline-block hover:bg-yellow-300'>Read Full Review</p>
+//                     <p className='font-light'>Michelle Messenger Garrett Garrett Public Relations</p>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
 
-export default Slider
+// export default Slider
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Fifthdown from '../components/Fifthdown'
 import Fourthdown from '../components/Fourthdown'
 import Navbar from '../components/Navbar'
-import Slider from '../components/Slider'
 import Thirdown from '../components/Thirdown'
 import Upperslider from '../components/Upperslider'
 
